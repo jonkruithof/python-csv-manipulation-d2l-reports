0bae34b56fcb6046ca179d343b43deb966be3f5c
jonkruithof <jon.kruithof@gmail.com>
2015-02-25 12:04:56 -0500

    Adding initial Python script and sample CSV file

---
+++ lp_date.py
@@ -0,0 +1,54 @@
+# author: Jon Kruithof kruithj@mcmaster.ca
+# date: Feb 24, 2015
+# script purpose: To take the dates people entered things in the Learning Portfolio and
+#                 calculate gaps in activity in days/minutes. Write those results to a CSV called
+#                 date.csv - which can then be used to graph/chart.
+
+import csv
+import datetime
+
+# create absolute references 
+
+sfile = "rawdata-nopush-date.csv"
+tfile = open("date.csv", "w")
+group = {}
+
+# open files
+
+target = csv.writer(tfile,dialect='excel')
+
+with open(sfile,'rU') as source:
+        
+    inread = csv.reader(source)
+
+# iterate through file  
+
+    for row in inread:
+        
+        username = row[2]
+        combo_date = row[0] + " " + row[1] 
+        entry_d = datetime.datetime.strptime(combo_date,"%Y-%m-%d %I:%M %p")
+        
+        if username in group:
+            
+# calculate the difference between the two dates
+
+            difference = entry_d - group.get(username)
+         #   print combo_date
+            entry = [username,str(difference)]
+
+# write the latest entry to the dictionary and write the username:difference pair to the CSV
+
+            target.writerow(entry)
+            group.update({username:entry_d}) 
+            
+        else:
+            
+           group.update({username:entry_d}) 
+
+print "DONE"
+
+#close the files
+
+source.close()
+tfile.close()
